f57da29604252c693a0befa7dc08a953be14d8e7
Eduardo Santos <eduardosantoshf@gmail.com>
2021-05-31 18:28:30 +0100

    Major changes to make spring boot project

---
+++ deliveries-engine/deliveries-engine/src/main/java/deliveries_engine/DeliveriesEngineApplication.java
@@ -0,0 +1,11 @@
+package deliveries_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveriesEngineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveriesEngineApplication.class, args);
+    }
+}
