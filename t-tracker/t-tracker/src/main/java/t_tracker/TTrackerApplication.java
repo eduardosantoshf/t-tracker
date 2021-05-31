4dbe6ddbe4499207ca3014119c3b607be30c6069
Eduardo Santos <eduardosantoshf@gmail.com>
2021-05-31 18:31:49 +0100

    Major changes to make spring boot project

---
+++ t-tracker/t-tracker/src/main/java/t_tracker/TTrackerApplication.java
@@ -0,0 +1,11 @@
+package t_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TTrackerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TTrackerApplication.class, args);
+    }
+}
