131f711397ae44399f9faf8df2463888285039df
bastos-01 <p.bastos112@gmail.com>
2021-06-04 00:22:56 +0100

    rider controller methods initiated

--- deliveries-engine/deliveries-engine/src/main/java/deliveries_engine/controller/RiderController.java
+++ deliveries-engine/deliveries-engine/src/main/java/deliveries_engine/controller/RiderController.java
@@ -3,14 +3,23 @@ package deliveries_engine.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import deliveries_engine.model.Rider;
+
 @RestController
 @RequestMapping("/rider")
 public class RiderController {
 
-    @PostMapping(value = "/{location}")
+    @PostMapping(value = "/signup", produces = "application/json")
+    public void registerRider(@RequestBody Rider rider){
+    }
+
+
+    @PostMapping(value = "/location/{location}")
     public void updateLocation(@PathVariable(value = "location") String location){
+    }
 
-
+    @PostMapping(value = "/status/{status}")
+    public void updateStatus(@PathVariable(value = "status") int status){
     }
 
 
