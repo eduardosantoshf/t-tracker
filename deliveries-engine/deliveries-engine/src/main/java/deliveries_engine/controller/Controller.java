748336b01f5e5fc7145cd51a6e33939a13711a33
bastos-01 <p.bastos112@gmail.com>
2021-06-04 00:39:45 +0100

    stats controller

--- deliveries-engine/deliveries-engine/src/main/java/deliveries_engine/controller/Controller.java
+++ deliveries-engine/deliveries-engine/src/main/java/deliveries_engine/controller/Controller.java
@@ -4,5 +4,10 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Controller {
+
+    @GetMapping(value = "/stats", produces = "application/json")
+    public void getStats(){
+        
+    }
     
 }
