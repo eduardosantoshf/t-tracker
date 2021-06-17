f8c85ab59cb356c1ba663a20aafa64682442283a
moraisandreua <48691769+moraisandreua@users.noreply.github.com>
2021-06-17 10:42:38 +0100

    update authentication
    - auto check if user is logged or not

--- t-tracker/web/assets/js/auth.js
+++ t-tracker/web/assets/js/auth.js
@@ -1,3 +1,35 @@
+function checkToken(){
+    function getCookie(name) {
+        var dc = document.cookie;
+        var prefix = name + "=";
+        var begin = dc.indexOf("; " + prefix);
+        if (begin == -1) {
+            begin = dc.indexOf(prefix);
+            if (begin != 0) return null;
+        }
+        else
+        {
+            begin += 2;
+            var end = document.cookie.indexOf(";", begin);
+            if (end == -1) {
+            end = dc.length;
+            }
+        }
+        // because unescape has been deprecated, replaced with decodeURI
+        //return unescape(dc.substring(begin + prefix.length, end));
+        return decodeURI(dc.substring(begin + prefix.length, end));
+    }
+    
+    var myCookie = getCookie("sessionKey");
+
+    if (myCookie == null) {
+        window.location.href='/login.html';
+    }
+    else {
+        // do cookie-exists stuff
+    }
+}
+
 function signupclient(){
     let username=$("#usernameTxt").val();
     let password=$("#passwordTxt").val();
