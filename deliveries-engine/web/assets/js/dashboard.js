4a10f752f12f6541ee48c60fdc6300af6d803b69
moraisandreua <48691769+moraisandreua@users.noreply.github.com>
2021-06-20 12:20:28 +0100

    UPDATE list all deliveries by rider

--- deliveries-engine/web/assets/js/dashboard.js
+++ deliveries-engine/web/assets/js/dashboard.js
@@ -57,3 +57,44 @@ function changeStateStyle(state){
             $("#riderDeliveringBadge").text("Warning: Cannot get your current state");
     }
 }
+
+function getAllDeliveries(){
+    let riderID=localStorage.getItem("riderID") //
+
+    fetch('http://localhost:8080/rider/deliveries/' + riderID, { headers: { 'Content-Type': 'application/json', 'Authorization': "Bearer " + getCookie("sessionKey-rider") }, method: 'get' }).then(data => {
+        if (data.status == 200) {
+            data = data.json();
+            
+            Promise.all([data]).then(data => {
+                let arr=data[0];
+
+                arr.map((obj) => {
+                    let tr = document.createElement('tr');
+
+                    let td_id = document.createElement('td');
+                    td_id.innerText=obj.id;
+
+                    let td_nome = document.createElement('td');
+                    td_nome.innerText=obj.nome;
+
+                    let td_status = document.createElement('td');
+                    let div_status = document.createElement('div');
+                    div_status.className="statusPending";
+                    div_status.innerText=obj.status;
+                    // statusDelivering
+                    //statusComplete
+                    td_status.appendChild(div_status)
+                    let td_comissao = document.createElement('td');
+
+
+
+                    tr.appendChild(td_id);
+                    tr.appendChild(td_nome);
+                    tr.appendChild(td_status);
+                    tr.appendChild(td_comissao);
+                    document.getElementById('deliveriesList').appendChild(tr);
+                });
+            });
+        }
+    });
+}
